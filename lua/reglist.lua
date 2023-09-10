59783caa03167f45f3791390ab89cbef984852af
Konstantin Merker <konstantin@semerker.de>
2023-09-10 23:59:42 +0200

    Fixed bug: it now considers regnames from config

--- lua/reglist.lua
+++ lua/reglist.lua
@@ -164,23 +164,7 @@ function M.setup(config)
         M.set_default_keymaps(config.default_mappings)
     end
     if (config.regnames) then
-        config.regnames = {
-            "a",  --  1
-            "b",  --  2
-            "c",  --  3
-            "d",  --  4
-            "e",  --  5
-            "f",  --  6
-            "g",  --  7
-            "h",  --  8
-            "i",  --  9
-            "j",  -- 10
-            "l",  -- 11
-            "k",  -- 12
-            "m",  -- 13
-            "n",  -- 14
-            "o",  -- 15
-        }
+        M.regnames = config.regnames
     end
 end
 
